ab9e05e248c790c00ff08adc209a82b68a799247
Nurdaulet <baglanovbaglan227@gmail.com>
2024-11-02 18:32:04 +0500

    started from 0

--- models/prediction.py
+++
@@ -1,6 +0,0 @@
-def predict_usage(model, input_features, scaler):
-    """Predicts bus usage."""
-    numerical_features = ['timestamp_unix', 'hour', 'minute', 'dayofweek', 'season', 'usage_lag_1', 'usage_lag_24']
-    input_features = input_features.copy()
-    input_features[numerical_features] = scaler.transform(input_features[numerical_features])
-    return model.predict(input_features)
